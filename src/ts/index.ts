111f956948c5b2b7bb4ebfcee17ff9a108a5d347
shiverazul <shiverazul@gmail.com>
2020-05-08 22:36:11 +0200

    Creacion

---
+++ src/ts/index.ts
@@ -0,0 +1,20 @@
+//Copy html to dist
+require('file-loader?name=[name].[ext]!../index.html');
+
+//Importar imagenes: Al hacer el build se creara la imagen en dist/assets
+//const asrc = require('../img/fondo-luna-Carlos1.jpg);
+//console.log("Imagen creada en: " + asrc);
+
+//Importar clase
+import { Person } from './class.person';
+
+//Llamar clase
+const p = new Person("Jon Doe");
+console.log("My name is " + p.name);
+
+
+var f = new Date();
+var dd = String(f.getDate()).padStart(2, '0');
+var mm = String(f.getMonth() + 1).padStart(2, '0'); //January is 0!
+document.getElementById('fecha').innerHTML = dd + "-" + mm + "-" + f.getFullYear();
+		
---
+++ webpack.dev.config.js
@@ -0,0 +1,72 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require("path")
+module.exports = {
+  entry: ['./src/ts/index.ts', './src/scss/style.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true
+  },
+  module: {
+    rules: [
+      // Extracts the compiled CSS from the SASS files defined in the entry
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // Interprets CSS
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'sass-loader' // 将 Sass 编译成 CSS
+          }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      },
+      //Imagenes
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '/assets/[name].[ext]',
+          esModule: false,
+        },
+      }
+    ],
+  },
+  plugins: [
+    // Where the compiled SASS is saved to
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+      allChunks: true,
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          safe: true
+        }
+      })
+    ]
+  },
+};
